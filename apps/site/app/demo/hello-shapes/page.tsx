6eefd6aac5521f5af58666b51f07fb58c61dbc1b
The Dude <mohsen.behvar@gmail.com>
2025-06-11 20:21:44 +0200

    feat: track lesson progress locally

--- apps/site/app/demo/hello-shapes/page.tsx
+++ apps/site/app/demo/hello-shapes/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { useEffect } from 'react'
 import Whiteboard from '../../components/Whiteboard'
 import SquareBuilder from '@madts/demos/area-perimeter'
+import { useProgress } from '@madts/canvas-core'
 
 export default function HelloShapesPage() {
+  const { markComplete } = useProgress()
+  useEffect(() => {
+    markComplete('hello-shapes')
+  }, [markComplete])
+
   return (
     <main className="flex min-h-screen w-screen flex-col items-center gap-4 p-4">
       <div className="relative h-[80vh] w-full border">
